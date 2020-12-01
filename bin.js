78670ccb56cbf72ad2f47c369c6e1efb0b6cfe7a
Dafiul Haq <dafiulhaq97@gmail.com>
2020-12-01 19:41:32 +0700

    replace meow with minimist and change indent to 2

--- bin.js
+++ bin.js
@@ -1,9 +1,25 @@
 #!/usr/bin/env node
 
-const meow = require("meow");
+const argv = require('minimist')(process.argv.slice(2));
 const batchSvg2Img = require(".");
 
-const cli = meow(`
+function pickProperties(obj, props) {
+  let newObj = {};
+
+  props.forEach((prop) => {
+    if (obj.hasOwnProperty(prop)) {
+      newObj[prop] = obj[prop];
+    }
+  });
+
+  return newObj;
+}
+
+if (argv.h) argv.help = argv.h;
+if (argv.f) argv.format = argv.f;
+
+if (argv.help === true) {
+  console.log(`
 Usage:
   $ npx batch-svg2img [source_folder] [dest_folder]
 
@@ -14,37 +30,19 @@ Arguments:
   --height
   --quality
   --preserveAspectRatio
-`, {
-    flags: {
-        format: {
-            type: "string",
-            alias: "f",
-            default: "png"
-        },
-        width: {
-            type: "number"
-        },
-        height: {
-            type: "number"
-        },
-        preserveAspectRatio: {
-            type: "string"
-        },
-        quality: {
-            type: "number"
-        }
-    }
-});
-
-if (cli.flags.h) {
-    cli.showHelp();
-} else if (cli.input.length === 0) {
-    console.error("Source folder not specified")
-} else if (!["png", "jpg", "jpeg"].includes(cli.flags.format)) {
-    console.error("Only 'png', 'jpg', and 'jpeg' supported")
+  `);
+} else if (argv._.length === 0) {
+  console.error("Source folder not specified");
+} else if (argv.format && !["png", "jpg", "jpeg"].includes(argv.format)) {
+  console.error("Only 'png', 'jpg', and 'jpeg' format supported");
 } else {
-    const src = cli.input[0];
-    const dest = cli.input[1] || "dest";
+  const src = argv._[0];
+  const dest = argv._[1] || "dest";
 
-    batchSvg2Img(src, dest, cli.flags);
+  argv.format = argv.format || "png";
+
+  batchSvg2Img(src, dest, pickProperties(
+    argv,
+    ["format", "width", "height", "quality", "preserveAspectRatio"]
+  ));
 }
--- main.js
+++ main.js
@@ -5,28 +5,28 @@ const mkdirp = require("mkdirp");
 const svg2img = require("svg2img");
 
 function batchSvg2Img(srcDir, destDir, options = {}) {
-    const cwd = process.cwd();
-    const svgPattern = path.join(cwd, srcDir, "*.svg").replace(/\\/g, "/");
+  const cwd = process.cwd();
+  const svgPattern = path.join(cwd, srcDir, "*.svg").replace(/\\/g, "/");
 
-    fg(svgPattern).then((entries) => {
-        mkdirp.sync(path.join(cwd, destDir));
+  fg(svgPattern).then((entries) => {
+    mkdirp.sync(path.join(cwd, destDir));
 
-        entries.forEach((filePath) => {
-            svg2img(filePath, options, (err, buffer) => {
-                if (err) {
-                    return console.log(err);
-                }
+    entries.forEach((filePath) => {
+      svg2img(filePath, options, (err, buffer) => {
+        if (err) {
+          return console.log(err);
+        }
+
+        const fileName = path.basename(filePath);
+        const outputFileName = fileName.replace(/\.svg$/, `.${options.format}`);
+        const outputFilePath = path.join(cwd, destDir, outputFileName);
+
+        console.log(`Writing "${outputFilePath}"`);
         
-                const fileName = path.basename(filePath);
-                const outputFileName = fileName.replace(/\.svg$/, `.${options.format}`);
-                const outputFilePath = path.join(cwd, destDir, outputFileName);
-
-                console.log(`Writing "${outputFilePath}"`);
-                
-                fs.writeFileSync(outputFilePath, buffer);
-            });
-        });
+        fs.writeFileSync(outputFilePath, buffer);
+      });
     });
+  });
 }
 
 module.exports = batchSvg2Img;
