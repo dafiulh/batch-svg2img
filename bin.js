24ddf167255009eb4a61b1394a0dac490f4c1aa0
Dafiul Haq <dafiulhaq97@gmail.com>
2020-12-18 16:15:15 +0700

    better code & change sample svg stroke to red because the default color is black so it's not visible when using a format other than png

--- bin.js
+++ bin.js
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
 
-const argv = require('minimist')(process.argv.slice(2));
 const batchSvg2Img = require('.');
+const minimist = require('minimist');
+const argv = minimist(process.argv.slice(2), {
+  alias: {
+    help: 'h',
+    format: 'f'
+  },
+  default: {
+    format: 'png'
+  }
+});
 
 function pickProperties(obj, props) {
   const newObj = {};
@@ -15,17 +24,14 @@ function pickProperties(obj, props) {
   return newObj;
 }
 
-if (argv.h) argv.help = argv.h;
-if (argv.f) argv.format = argv.f;
-
-if (argv.help === true) {
+if (argv.help) {
   console.log(`
 Usage:
   $ npx batch-svg2img [source_folder] [dest_folder]
 
 Arguments:
-  -H, --help
-  -F, --format
+  -h, --help
+  -f, --format
   --width
   --height
   --quality
@@ -33,16 +39,17 @@ Arguments:
   `);
 } else if (argv._.length === 0) {
   console.error('Source folder not specified');
-} else if (argv.format && !['png', 'jpg', 'jpeg'].includes(argv.format)) {
+} else if (!['png', 'jpg', 'jpeg'].includes(argv.format)) {
   console.error('Only \'png\', \'jpg\', and \'jpeg\' format supported');
 } else {
   const src = argv._[0];
   const dest = argv._[1] || 'dest';
 
-  argv.format = argv.format || 'png';
-
-  batchSvg2Img(src, dest, pickProperties(
-      argv,
-      ['format', 'width', 'height', 'quality', 'preserveAspectRatio']
-  ));
+  batchSvg2Img(src, dest, pickProperties(argv, [
+    'format',
+    'width',
+    'height',
+    'quality',
+    'preserveAspectRatio'
+  ]));
 }
