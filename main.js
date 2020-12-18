43afa76cfc324bc20c82327f0cb5e72421076260
Dafiul Haq <dafiulhaq97@gmail.com>
2020-12-18 16:11:41 +0700

    remove fast-glob

--- main.js
+++ main.js
@@ -1,33 +1,43 @@
-const path = require('path');
 const fs = require('fs');
-const fg = require('fast-glob');
+const path = require('path');
 const svg2img = require('svg2img');
 
 function batchSvg2Img(srcDir, destDir, options = {}) {
   const cwd = process.cwd();
   srcDir = path.join(cwd, srcDir);
   destDir = path.join(cwd, destDir);
-  const svgPattern = path.join(srcDir, '*.svg').replace(/\\/g, '/');
 
-  fg(svgPattern).then((entries) => {
-    if (!fs.existsSync(destDir)) {
-      fs.mkdirSync(destDir);
+  const svgFilesPath = fs.readdirSync(srcDir).filter((file) => {
+    const filePath = path.join(srcDir, file);
+
+    if (fs.statSync(filePath).isDirectory()) {
+      return false;
     }
 
-    entries.forEach((filePath) => {
-      svg2img(filePath, options, (err, buffer) => {
-        if (err) {
-          return console.log(err);
-        }
+    return filePath.endsWith('.svg');
+  }).map((file) => {
+    return path.join(srcDir, file);
+  });
 
-        const fileName = path.basename(filePath);
-        const outputFileName = fileName.replace(/\.svg$/, `.${options.format}`);
-        const outputFilePath = path.join(destDir, outputFileName);
+  console.log(svgFilesPath);
 
-        console.log(`Writing "${outputFilePath}"`);
+  if (!fs.existsSync(destDir)) {
+    fs.mkdirSync(destDir);
+  }
 
-        fs.writeFileSync(outputFilePath, buffer);
-      });
+  svgFilesPath.forEach((filePath) => {
+    svg2img(filePath, options, (err, buffer) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      const fileName = path.basename(filePath);
+      const outputFileName = fileName.replace(/\.svg$/, `.${options.format}`);
+      const outputFilePath = path.join(destDir, outputFileName);
+
+      console.log(`Writing "${outputFilePath}"`);
+
+      fs.writeFileSync(outputFilePath, buffer);
     });
   });
 }
